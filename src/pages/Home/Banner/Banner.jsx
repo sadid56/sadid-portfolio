abdd1dc2763aa44db730ec91ccc045a2a40cae91
sadid56 <sadidhasan56@gmail.com>
2023-12-12 07:39:23 +0600

    added a banner button

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -10,6 +10,7 @@ import {
   FaGithub,
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
 import helloAnimation from "../../../assets/lottie-animation/hello.json";
 import "./banner.css";
 import Aos from "aos";
@@ -17,21 +18,21 @@ import 'aos/dist/aos.css';
 import { useEffect } from "react";
 import resume from '../../../assets/images/resume.png'
 const Banner = () => {
-  useEffect(() => {
-    Aos.init({
-      duration: 1500, 
-      // offset: 200,
-    });
-  }, []);
+  // useEffect(() => {
+  //   Aos.init({
+  //     duration: 1500, 
+  //     // offset: 200,
+  //   });
+  // }, []);
   return (
     <div className="w-full bg-grad py-5 px-5 pt-24 md:pt-0">
-      <div className="flex flex-col-reverse md:flex-row h-screen pt-10 items-center gap-10 max-w-6xl mx-auto">
-        <div className="text-white flex-1 md:w-1/2 space-y-3">
+      <div className="flex  flex-col-reverse md:flex-row h-screen pt-10 items-center gap-10 max-w-6xl mx-auto">
+        <div className="text-white flex-1  md:w-1/2 space-y-3">
           <div className=" font-semibold uppercase flex items-center gap-3">
             <div className="w-16 bg-white rounded-md">
               <Lottie animationData={helloAnimation} />
             </div>
-            <h2>There</h2>
+            <h2>There,</h2>
           </div>
           <h1 className="text-4xl font-bold">
             I'am <span className="text-[#03e9f4]">Sadid</span>,
@@ -65,40 +66,27 @@ const Banner = () => {
               cursorStyle="."
             />
           </h1>
-         <div className="">
-         <a href={resume} download target="_blank" className="relative inline-flex bg-transparent items-center justify-start py-2 pl-4 pr-12 overflow-hidden font-semibold text-slate-400  transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 border border-slate-400 group">
-              <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-                <svg
-                  className="w-5 h-5 text-[#03e9f4]"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
-                </svg>
-              </span>
-              <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-                <svg
-                  className="w-5 h-5 text-slate-400"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
-                </svg>
-              </span>
-              <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-[#03e9f4]">
-                Download Resume
-              </span>
+         <div className="py-8 relative w-full">
+            <a href={resume} target="_blank" download className="button absolute left-[70px]">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              Resume
             </a>
+            <ScrollLink  to="contact"
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={-50} className="button2 absolute left-[220px] cursor-pointer">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              Hair me
+            </ScrollLink>
+          
+         
            
          </div>
           <div className="flex items-center gap-2">
